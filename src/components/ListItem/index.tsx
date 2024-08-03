874683d0c1fd1fde59668c471897689508a71536
Katiana Hanisch <katianahanisch@gmail.com>
2024-08-03 19:09:47 -0400

    resolvendo erro de build

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -16,7 +16,6 @@ const ListItem = ({ tarefa, onChange }: Props) => {
         onChange={(e) => onChange(tarefa.id, e.target.checked)}
       />
       <label>{tarefa.name}</label>
-      {console.log(tarefa.done)}
     </C.Container>
   );
 };
